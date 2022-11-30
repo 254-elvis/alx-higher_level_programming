0d9dc0c648b7c1478730fe190df189442b2fa429
254-elvis <mureithielvis18@gmail.com>
2022-11-30 05:04:07 -0800

    new

--- 0x01-python-if_else_loops_functions/13-insert_number.c
+++ 0x01-python-if_else_loops_functions/13-insert_number.c
@@ -1,39 +1,49 @@
-/*
- * File: 13-insert_number.c
- * Auth: Brennan D Baraban
- */
-
 #include "lists.h"
+#include <stddef.h>
+#include <stdlib.h>
 
 /**
- * insert_node - Inserts a number into a sorted singly-linked list.
- * @head: A pointer the head of the linked list.
- * @number: The number to insert.
+ * insert_node - function that inserts a number to a singly linked list.
+ * @head: pointer to pointer to head of the list.
+ * @number: value of the node to insert.
  *
- * Return: If the function fails - NULL.
- *         Otherwise - a pointer to the new node.
+ * Return: the address of the new node, or NULL if it failed.
  */
+
 listint_t *insert_node(listint_t **head, int number)
 {
-	listint_t *node = *head, *new;
-
-	new = malloc(sizeof(listint_t));
-	if (new == NULL)
+	listint_t *new_node, *traverse;
+	unsigned int idx = 0, i = 0;
+	/* if there is no list return null */
+	if (head == NULL)
 		return (NULL);
-	new->n = number;
-
-	if (node == NULL || node->n >= number)
+	/* create the new node */
+	new_node = malloc(sizeof(listint_t));
+	if (new_node == NULL)
+		return (NULL);
+	/* access the n field of the new_node and initialize it as n */
+	new_node->n = number;
+	/* check if idx = 0 */
+	if (idx == 0)
 	{
-		new->next = node;
-		*head = new;
-		return (new);
+		/* access the next field of new_node and assign it as first node */
+		new_node->next = *head;
+		*head = new_node;
+		return (new_node);
+	}
+	/* make traverse be the value at head */
+	traverse = *head;
+	while (i != idx - 1 && traverse != NULL)
+	{
+		traverse = traverse->next;
+		i++;
 	}
 
-	while (node && node->next && node->next->n < number)
-		node = node->next;
-
-	new->next = node->next;
-	node->next = new;
-
-	return (new);
+	if (i == idx - 1 && traverse != NULL)
+	{
+		new_node->next = traverse->next;
+		traverse->next = new_node;
+		return (new_node);
+	}
+	return (NULL);
 }
