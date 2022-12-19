df1cecfe9433848448557dc982bc2fe838bc827d
254-elvis <mureithielvis18@gmail.com>
2022-12-19 03:32:45 -0800

    Safe list printing

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,14 +1,24 @@
 #!/usr/bin/python3
+
 def safe_print_list(my_list=[], x=0):
-    index = 0
-    while True:
+    """
+    Prints x elements of a list.
+    All elements must be printed on the same line followed by a new line.
+    You have to use try: / except:
+    You are not allowed to import any module
+    You are not allowed to use len()
+        Args:
+            my_list: list
+            x: number of elements to print.
+        Returns: the real number of elements printed.
+    """
+    count = 0
+    for item in range(x):
         try:
-            if index < x:
-                print(my_list[index], end='')
-                index += 1
-            else:
-                print()
-                return index
+            print(my_list[item], end='')
         except IndexError:
-            print()
-            return 
+            break
+        else:
+            count += 1
+    print()
+    return count
